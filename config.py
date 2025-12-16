6da605a08cd9b06c83044693ed5f6e2b3b4d170f
RoxxOP <king25258069@gmail.com>
2025-12-16 07:46:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,10 @@
 import os
 
-# ----------------- TELEGRAM CREDENTIALS ----------------- #
 API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
-
-# ----------------- OWNER & ADMIN ----------------- #
 OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
-
-# ----------------- DATABASE & LOGGING ----------------- #
 MONGO_URL = os.environ.get("MONGO_URL")
 LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "0"))
-
-# ----------------- AI API KEYS ----------------- #
 GIT_TOKEN = os.environ.get("GIT_TOKEN")
